ad71c3139e4aa0578e47a3e891db79859e526cb3
softtech2005 <enginemustobey@gmail.com>
2024-04-01 14:53:09 -0700

    edit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,6 +11,7 @@ def fetch_commits(repo, owner):
     """Fetches the last 10 commits of the repository by the owner"""
     url = f"https://api.github.com/repos/{owner}/{repo}/commits"
     params = {'per_page': 10}
+    response = requests.get(url, params=params)
     if response.status_code == 200:
         commits = response.json()
         for commit in commits:
@@ -25,7 +26,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: ./100-github_commits.py <repository> <owner>")
         sys.exit(1)
-    
+
     repo = sys.argv[1]
     owner = sys.argv[2]
     fetch_commits(repo, owner)
